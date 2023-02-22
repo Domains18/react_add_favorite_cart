503cbaa74e3742b4847fde31d3d2e0da93acdd0d
Domains18 <gibsonsgibson88@gmail.com>
2023-02-23 02:46:41 +0300

    server

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
 const colors = require('colors');
+const { errorHandler } = require('./helpers/errorHandler');
+const port = process.env.PORT || 3000;
 
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
+
+app.use('/api/v1', async (req, res, next) => {
+    res.status(200).json({
+        message: 'Welcome to the API'
+    })
+});
+
+app.use('/api/v1', async (req, res, next) => {
+    res.status(200).json({
+        message: 'Welcome to the API'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`.yellow.bold);
+
+})
