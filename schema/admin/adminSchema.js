8f5af4fb20621a16144dfaa9750cccaa010b478d
Domains18 <gibsonsgibson88@gmail.com>
2023-02-23 23:32:33 +0300

    adminschema

---
+++ schema/admin/adminSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+
+const adminSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+
+
+
+
+module.exports = mongoose.model('Admin', adminSchema);
+
