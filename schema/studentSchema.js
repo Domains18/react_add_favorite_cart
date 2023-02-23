371c04b7fb1de4c79eeb958eb225b6a0d0954dae
Domains18 <gibsonsgibson88@gmail.com>
2023-02-23 03:52:44 +0300

    student schema

---
+++ schema/studentSchema.js
@@ -0,0 +1,60 @@
+const schema = require('mongoose').Schema;
+
+const studentSchema = new schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    prevSchool: {
+        type: String,
+        required: true
+    },
+    grade: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    allergies: {
+        type: String,
+        required: true
+    },
+    medicalConditions: {
+        type: String,
+        required: true
+    },
+    parentName: {
+        type: String,
+        required: true
+    },
+    parentEmail: {
+        type: String,
+        required: true
+    },
+    parentPhone: {
+        type: String,
+        required: true
+    },
+    career: {
+        type: String,
+        required: true
+    },
+    parentTeacher: {
+        type: String,
+        required: true,
+        ref: 'Teacher'
+    },
+});
+
+const Student = module.exports = require('mongoose').model('Student', studentSchema);
+module.exports = Student;
