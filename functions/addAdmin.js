12699d0f6ed78b97fc9b6472ccc622d8d8d02771
Domains18 <gibsonsgibson88@gmail.com>
2023-02-25 19:17:30 +0300

    admin functionality

--- functions/addAdmin.js
+++ functions/addAdmin.js
@@ -4,4 +4,11 @@ const expressAsyncHandler = require('express-async-handler');
 
 const addTeacherAdmin = expressAsyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
+    if(!name, !email, !password) {
+        res.status(400);
+        throw new Error('Please fill all the fields');
+    }
+
+    //admin must be a teacher
+    const checkTeacher = await Teacher.findOne({ email });
 });
