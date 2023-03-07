92dae9fe9ce31c08c6a91779468360eb0b6cfb45
Domains18 <gibsonsgibson88@gmail.com>
2023-03-07 20:41:02 +0300

    protected routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import DefaultLayout from './layout/DefaultLayout';
 import GuestLayout from './layout/GuestLayout';
-import Home from './components/Home';
 import About from './components/About';
 import Dashboard from './components/Dashboard';
 import Login from './views/Login';
 import Signup from './views/Signup';
 import NotFound from './views/NotFound.js';
+import App from './App';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <DefaultLayout />,
         children: [
-            { path: '/', element: <Home /> },
+            { path: '/', element: <App /> },
             { path: '/about', element: <About /> },
             { path: '/dashboard', element: <Dashboard /> },
         ]
@@ -24,8 +24,10 @@ const router = createBrowserRouter([
         element: <GuestLayout />,
         children: [
             { path: '/login', element: <Login /> },
-            { path: '/Signup', element: <Register /> },
+            { path: '/Signup', element: <Signup /> },
         ]
     },
     { path: '*', element: <NotFound />}
 ]);
+
+export default router;
