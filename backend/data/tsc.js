5bb4377e92af4961fb94e9af919d3e81366712d9
Domains18 <gibsonsgibson88@gmail.com>
2023-03-07 20:57:12 +0300

    cleanup

--- backend/data/tsc.js
+++
@@ -1,9 +0,0 @@
-// const Teacher = require('../schema/teacherSchema');
-// const expressAsyncHandler = require('express-async-handler');
-
-
-
-
-// const checkTeacher = expressAsyncHandler(async (req, res, next) => {
-//     // find a way to verify if the te
-// });
