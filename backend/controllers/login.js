557a46e4a7cac0bb5a6d28d1d0084c5b36d1d65a
Domains18 <gibsonsgibson88@gmail.com>
2023-03-09 01:23:25 +0300

    login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,7 +1,32 @@
 const Admin = require("../models/adminSchema");
 const bcrypt = require("bcryptjs");
-
+const asyncHandler = require('express-async-handler');
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
+
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({ email });
+    if (user && (await bcrypt.compare(password, admin.password))) {
+        res.json({
+            _id: admin.id,
+            name: admin.names,
+            email: admin.email,
+        });
+
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials, Acces denied');
+    }
+});
+
+
+const adminInfo = asyncHandler(async (req, res) => {
+    res.status(200).json(req.user);
+});
+
+
+module.exports = { login, adminInfo };
