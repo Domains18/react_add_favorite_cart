386c1eaf92597ebe88b01209cf2923decb3deec7
Domains18 <gibsonsgibson88@gmail.com>
2023-03-08 13:58:30 +0300

    models

--- backend/controllers/registration.js
+++ backend/controllers/registration.js
@@ -1,21 +1,23 @@
 
 // Path: backend/controllers/registration.js
 const asyncHandler = require('express-async-handler');
-const Student = require('../schema/studentSchema');
-const Parent = require('../schema/parentSchema');
-const Admin = require('../schema/adminSchema');
-const Class = require('../schema/classSchema');
-const Subject = require('../schema/subjectSchema');
-const Teacher = require('../schema/teacherSchema');
+const Student = require('../models/studentSchema');
+const Parent = require('../models/parentSchema');
+const Admin = require('../models/adminSchema');
+const Class = require('../models/classSchema');
+const Subject = require('../models/subjectSchema');
+const Teacher = require('../models/teacherSchema');
 
 
 // @desc    Register a new student
 // @route   POST /api/registration/student
 // @access  Public
+//**@Domains18 */
 const registerStudent = asyncHandler(async (req, res) => {
     const { id, Fname, Lname, gender, dob, classId, isActive, joinDate, createdAt, updatedAt, createdBy } = req.body;
     if(!id || !Fname || !Lname || !gender || !dob || !classId || !isActive || !joinDate || !createdAt || !updatedAt || !createdBy) {
         res.status(400);
         throw new Error('All fields are required');
     }
+    
 });
