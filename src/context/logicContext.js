d4e9f2998b515e47ff416c247069577b0f667303
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-19 14:14:32 +0300

    logic

--- src/context/logicContext.js
+++ src/context/logicContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { Children, createContext, useContext, useState } from "react";
 
 
 import { useStorage } from "../hooks/storage";
@@ -8,6 +8,68 @@ import Cart from "../components/Cart";
 const logicContext = createContext();
 
 
-export function shoppingCartProvider (){
-    
-}
+export function shoppingCartProvider() {
+    const [cartItems, setCartItems] = useStorage("cartItems", []);
+    const cartQuantity = cartItems.reduce((quantity, item) => quantity + item.quantity, 0);
+    const [cartOpen, setCartOpen] = useState(false);
+
+    function getQuantity(id) {
+        return cartItems.find((item) => item.id === id)?.quantity || 0;
+    }
+    const openCart = () => setCartOpen(true);
+    const closeCart = () => setCartOpen(false);
+
+    //TODO: add item quantity
+    function addItemQuantity(id) {
+        setCartItems(currentItems => {
+            if (currentItems.find(item => item.id === id) === null) {
+                return [...currentItems, { id, quantity: 1 }]
+            } else {
+                return currentItems.map(item => {
+                    if (item?.id === id) {
+                        return { ...item, quantity: item.quantity + 1 }
+                    } else {
+                        return item;
+                    }
+                });
+            }
+        });
+    }
+    function removeItemQuantity(id) {
+        setCartItems(currentItems => {
+            if (currentItems.find(item => item.id === id)?.quantity === 1) {
+                return currentItems.filter(item => item.id !== id)
+            } else {
+                return currentItems.map(item => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity - 1 }
+                    } else {
+                        return item;
+                    }
+                });
+            }
+        });
+    };
+
+    function clearCart(id) {
+        setCartItems(currentItems => {
+            return currentItems.filter(item => item.id !== id);
+        });
+    }
+
+    return (
+        <logicContext.Provider value={{
+            getQuantity,
+            removeItemQuantity,
+            removeItemQuantity,
+            clearCart,
+            openCart,
+            closeCart,
+            cartItems,
+            cartQuantity
+        }}>{Children}
+            <Cart setCartOpen={setCartOpen} />
+        </logicContext.Provider>
+    )
+};
+
