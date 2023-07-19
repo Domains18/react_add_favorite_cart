986cc141de2dbde894ae683b0f443fe1de6eac20
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-20 00:04:46 +0300

    favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../index.css'
+
+
+
+
 
 const Favorites = () => {
+  const favorites = JSON.parse(localStorage.getItem('favorites'));
+  const removeFromFavorites = (productId) => {
+    const updatedFavorites = favorites.filter(
+      (favoriteProduct) => favoriteProduct.id !== productId
+    );
+
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    window.location.reload(); // Refresh the page to update the displayed favorites
+  };
+  
+
+  console.log(favorites);
   return (
-    <div>Favorites</div>
+    <>
+      <h1>Favorites</h1>
+      <div className='favorites'>
+        {
+          favorites.map((favorite, index) => (
+            <div key={index} className='card'>
+              <img src={favorite.image} alt={favorite.name} />
+              <div className="card-body">
+                <h2>{favorite.id}</h2>
+                <h3>{favorite.name}</h3>
+                <p>{favorite.description}</p>
+                <p>{favorite.price}</p>
+                <div className="buttons">
+                  <button onClick={()=> removeFromFavorites(favorite.id)}>Remove from Favorites</button>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
